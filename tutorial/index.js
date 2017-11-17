f3b65202185290299ead6ae7731d003e0336457e
Tim <Tim@141.19.158.87>
2017-11-17 09:52:39 +0100

    server

---
+++ tutorial/index.js
@@ -0,0 +1,142 @@
+/*
+*node.js webserver providing static and dynamic html files, a front-end api, database connection
+* TODO : Catch all possible error relating to mysql
+*/
+//console.log ('server is running');
+var express = require('express');
+var app = express();
+var bodyparser = require('body-parser');
+app.use(bodyparser.json());
+// SQL Connections
+var mysql = require('mysql');
+ var con = mysql.createConnection({
+    host: "141.19.141.151",
+  user: "t_schaefer",
+    password: "1610337",
+    database: "pearsec"
+  });
+ // End of SQL-Connection
+//Launch Server 
+var server = app.listen(3000, listening);
+function listening(){
+    console.log("listeninnnng");
+}
+//Provide static files
+app.use(express.static('public'));
+//API-GET Pfäde & Methoden:
+
+app.get('/allAssets', getAllAssets);
+//get RisikoJeGefährdung
+app.get('/risikoFurGefahrdung/:param', risikoFurGefahrdung);
+//get RisikoJeGefährdung
+app.get('/gesamtRisiko', getGesamtRisiko);
+
+function getAllAssets (req, res){
+    // SQL Part
+    var sqlResult;
+    
+      con.query("SELECT * FROM Assets", function (err, result, fields) {
+          if (err) throw err;
+          console.log(result);
+           sqlResult = result;
+           res.send(result);
+         
+      });
+      
+      
+      // END OF SQL PART
+      console.log(sqlResult);
+      
+//res.send(sqlResult);
+}
+
+app.post('/post', postDaten);
+
+function postDaten (req, res){
+    // SQL Part
+   /* var sql = "INSERT INTO test1 (t1ID, text) VALUES ('57', 'patrick')";
+  con.query(sql, function (err, result) {
+    if (err) throw err;*/
+
+    console.log("1 record inserted");
+    console.log(req.body);
+
+    var sql = ("INSERT INTO test1 (t1ID, text) VALUES (' " + req.body.zahl +"','" + req.body.text +"');");
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+    });
+
+//});
+};
+
+
+function risikoFurGefahrdung (req, res){
+    // working
+  
+     getRisikoFurEineGefahrung(req.params.param, (xy)=>{res.send((xy));});
+   
+}
+
+function getGesamtRisiko (req, res){
+     getGesamtRisiko2((xy)=>{console.log(xy);});   
+   
+          
+}
+
+function getRisikoFurEineGefahrung(giD, _callback){
+  var sqlB = "SELECT (Eintrittswahrscheinlichkeit * Schadenshohe) as erg FROM Gefährdungen where GID= \"" + giD + " \";";
+//copied
+con.query(sqlB, (err, result, fields) => {
+  if (err) throw err;
+  _callback(result[0]);
+});
+}
+
+function getGesamtRisiko2( _callback){
+  var sqlB = "SELECT KundenAssetID from Kunde1Assets; ";
+ // var sqlB = "SELECT * from AssetsZuGefährdungen ;";
+//copied
+con.query(sqlB, (err, result, fields) => {
+  if (err) throw err;
+ // console.log(result[0].GiD);
+ var counter;
+    var gesamtRisiko;
+ Object.keys(result).forEach(function(key) {
+ // console.log( result[key].KundenAssetID);
+     //hier durch die json iterieren und jeweils patricks sql befehl aufrufen und dnn alles zusammen addieren
+   //console.log(e.);
+   
+    getRisikoFurEinAsset(result[key].KundenAssetID, (xy)=>{console.log("done : " + xy);
+    counter++;
+    gesamtRisiko= gesamtRisiko+xy});
+   //ende des sql parts
+
+});
+console.log(gesamtRisiko);
+  _callback((gesamtRisiko/counter));
+});
+}
+function getRisikoFurEinAsset(KundenAssetID, _callback){
+
+  //größte Gefährdung für ein Asset suchen:
+  var sqlB = "SELECT MAX (a.Eintrittswahrscheinlichkeit*a.Schadenshohe) as erg from Gefährdungen a, Assets b, Kunde1Verbindungen c, Kunde1Assets d where a.GID = c.GID and b.AID = c.AID and b.AID = d.AID and d.KundenAssetID = \"" + KundenAssetID + " \";";
+  con.query(sqlB, (err, result, fields) => {
+    if (err) throw err;
+    //ist die Gefährdung größer gleich 15 wird der _callback aufgerufen, ansonsten wird der avg wert berechnet
+   // console.log(result[0].erg);
+    if (result[0].erg>=15){
+      //console.log(result[0].erg);
+      _callback(15);
+    }else{
+      // avg wert berechnen
+      var sqlB = "SELECT AVG (a.Eintrittswahrscheinlichkeit*a.Schadenshohe) as erg from Gefährdungen a, Assets b, Kunde1Verbindungen c, Kunde1Assets d where a.GID = c.GID and b.AID = c.AID and b.AID = d.AID and d.KundenAssetID = \"" + KundenAssetID + " \";";
+      con.query(sqlB, (err, result, fields) => {
+        if (err) throw err;
+        //console.log(result[0].erg);
+        _callback(result[0].erg);
+      });
+      //ende der avg wert berechnung
+    }
+
+  });
+}
---
+++ tutorial/public/formular1.js
@@ -0,0 +1,158 @@
+/**
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+(function() {
+  "use strict";
+
+  var ENTER_KEY_CODE = 13;
+  var queryInput, resultDiv, accessTokenInput;
+
+  window.onload = init;
+
+  function init() {
+    queryInput = document.getElementById("q");
+    resultDiv = document.getElementById("result");
+
+    queryInput.addEventListener("keydown", queryInputKeyDown);
+    window.init();
+  }
+
+
+  function queryInputKeyDown(event) {
+    if (event.which !== ENTER_KEY_CODE) {
+      return;
+    }
+
+    var value = queryInput.value;
+    queryInput.value = "";
+
+    createQueryNode(value);
+    var responseNode = createResponseNode();
+
+    sendText(value)
+      .then(function(response) {
+        var result;
+        try {
+          result = response.result.fulfillment.speech
+        } catch(error) {
+          result = "";
+        }
+//        setResponseJSON(response);
+        setResponseOnNode(result, responseNode);
+      })
+      .catch(function(err) {
+        setResponseJSON(err);
+        setResponseOnNode("Something goes wrong", responseNode);
+      });
+  }
+
+  function createQueryNode(query) {
+    var node = document.getElementById('eingabe');
+/*    var node1 = document.createElement('div');
+    node1.style.height = "40px";*/
+    node.className = "green rounded accent-1 mt-3 p-3 font-weight-bold";
+    node.innerHTML = query;
+  }
+
+  function createResponseNode() {
+    var node = document.getElementById("antwort");
+    node.className = "white rounded blue-text mt-2 p-3 font-weight-bold";
+    node.innerHTML = "...";
+//    resultDiv.appendChild(node);
+    return node;
+  }
+
+  function setResponseOnNode(response, node) {
+    node.innerHTML = response ? response : "[empty response]";
+    node.setAttribute('data-actual-response', response);
+  }
+
+/*  function setResponseJSON(response) {
+    var node = document.getElementById("jsonResponse");
+    node.innerHTML = JSON.stringify(response, null, 2);
+  }*/
+
+  function sendRequest() {
+
+  }
+
+
+})();
+
+"use strict";
+var left = document.getElementById("left-defaults");
+
+function interact(){
+  var antwort = document.getElementById("antwort");
+  var assets = document.getElementById("right-defaults");
+  var hide = document.createElement("p");
+  hide.setAttribute("id", "versteckt");
+
+
+  if(antwort.className == "white rounded blue-text mt-2 p-3 font-weight-bold"){
+    antwort.innerHTML = "Was für Maschinen haben Sie im Unternehmen?";
+    assets.innerHTML =  "<div>Maschine</div>" +
+                        "<div>Maschine</div>" +
+                        "<div>Maschine</div>" +
+                        "<div>Maschine</div>" +
+                        "<div>Maschine</div>" +
+                        "<div>Maschine</div>";
+
+    left.innerHTLM = "";
+    left.appendChild = hide;
+    return;
+  }
+
+    return;
+  }
+
+function abschicken(){
+  var bar = document.getElementById("bar");
+  var x = document.getElementById("marker");
+  var versteckt = document.getElementById("versteckt");
+
+    if (versteckt.innerHTML == "") {
+        x.style.display = "block";
+    } else {
+      bar.style.width = "15%";
+    }
+
+    alert(left.innerHTML);
+
+    
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', "/post", true);
+    xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+    xhr.responseType ='json';
+    xhr.send(datasend);
+}
+
+window.onload = function (){
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+      var obj = JSON.parse(xhttp.responseText);
+      for(var item in obj){
+        var el = document.createElement('div');
+        el.innerHTML = obj[item].Name;
+        el.setAttribute('id', obj[item].AID );
+        document.getElementById("right-defaults").appendChild(el);
+      }
+    }
+  };
+  xhttp.open("GET", "/allAssets", true);
+  xhttp.send();
+}

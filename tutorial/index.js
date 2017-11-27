c4eb148fd9f5e145945f8e72187cee3013737756
patstreit <1511579@stud.hs-mannheim.de>
2017-11-27 17:02:36 +0000

    gefixte methoden

--- tutorial/index.js
+++ tutorial/index.js
@@ -155,7 +155,7 @@ function getAllKundenAssetsAndPruffragen(req,res){
 
   var sqlResult;
   //var sqlBef ="SELECT a.KundenAssetID, a.AiD, a.Name, b.Kategorien FROM Kunde1Assets a, Assets b where a.AID = b.AID";
-  con.query("SELECT DISTINCT a.KundenAssetID, a.AiD, a.Name, b.Kategorien, d.PID, d.Prüffrage FROM Kunde1Assets a, Assets b, AssetsZuGefährdungen c, Prüffragen d, Kunde1Verbindungen e WHERE a.AID = b.AID AND a.Aid = c.AID AND a.KundenAssetID = e.KundenAssetID  AND c.PID = d.PID AND c.GID = e.GID ORDER BY  `d`.`PID`;", function (err, result, fields) {
+  con.query("SELECT DISTINCT a.KundenAssetID, a.AiD, a.Name, b.Kategorien, d.Prüffragen FROM Kunde1Assets a, Assets b, AssetsZuGefährdungen c, Maßnahmen d, Kunde1Verbindungen e WHERE a.AID = b.AID AND a.Aid = c.AID AND a.KundenAssetID = e.KundenAssetID  AND d.MID = e.MID AND c.GID = e.GID;", function (err, result, fields) {
     if (err) throw err;
     console.log(result);
     sqlResult = result;

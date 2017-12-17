dae2055e797e5fe665f4e974aaf3a76b536343d6
patstreit <1511579@stud.hs-mannheim.de>
2017-12-17 14:29:07 +0000

    änderung an testmethoden

--- tutorial/index.js
+++ tutorial/index.js
@@ -95,8 +95,8 @@ app.delete('/deleteAsset/:kaid', assetloschen);
 app.put('/massnahmeErledigt/:KAID/:MID',updateMaßnahmeErledigt);
 app.put('/massnahmeErledigtNegativ/:KAID/:MID',updateMaßnahmeErledigtnegativ);
 app.put('/resettest',resettest);
-app.put('/testabhaken',updateMaßnahmeErledigt);
-app.put('/testabhakennegativ', updateMaßnahmeErledigtnegativ);
+app.put('/testabhaken/:KAID/:MID',updateMaßnahmeErledigt);
+app.put('/testabhakennegativ/:KAID/:MID', updateMaßnahmeErledigtnegativ);
 ////////////////////////
 //Funktionen der get API
 ////////////////////////
